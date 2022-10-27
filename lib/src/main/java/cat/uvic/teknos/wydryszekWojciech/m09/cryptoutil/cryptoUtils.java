73eca4697a643022eacfe3fa808efea364c6d00a
wojciech wydryszek <wojciech.wydryszek@uvic.cat>
2022-10-27 13:51:51 +0200

    implement verify and signature medoths

--- lib/src/main/java/cat/uvic/teknos/wydryszekWojciech/m09/cryptoutil/cryptoUtils.java
+++ lib/src/main/java/cat/uvic/teknos/wydryszekWojciech/m09/cryptoutil/cryptoUtils.java
@@ -2,12 +2,18 @@ package cat.uvic.teknos.wydryszekWojciech.m09.cryptoutil;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.Base64;
 import java.util.Properties;
 
 public class cryptoUtils {
     /***
+     * Symmetric encryption algorithm
      * WydryszekWojciech implementation for encryption message encryption algorithm
      * @param plainText
      * @param password
@@ -54,6 +60,7 @@ public class cryptoUtils {
     }
 
     /***
+     * Symmetric encryption algorithm
      * WydryszekWojciech implementation for encryption message decrypt algorithm
      * @param cipherText
      * @param password
@@ -68,5 +75,64 @@ public class cryptoUtils {
         var decryptedTextBytes = cipher.doFinal(cipherText);
 
         return decryptedTextBytes;
+    }
+
+    /***
+     * Asymmetric signature algorithm
+     * WydryszekWojciech implementation for sign message
+     * @param message
+     * @return byte[]
+     * @throws KeyStoreException
+     * @throws IOException
+     * @throws CertificateException
+     * @throws NoSuchAlgorithmException
+     * @throws UnrecoverableKeyException
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     */
+    public byte[] sign(byte[] message) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, InvalidKeyException, SignatureException {
+
+        var keystore = KeyStore.getInstance("PKCS12");
+
+        keystore.load(new FileInputStream
+                ("app/src/main/resources/m09.p12"), "Teknos01.".toCharArray());
+
+        var privateKey = keystore.getKey
+                ("self_signed_ca", "Teknos01.".toCharArray());
+
+        var signer = Signature.getInstance("SHA256withRSA");
+
+        signer.initSign((PrivateKey) privateKey);
+        signer.update(message);
+
+        var signature = signer.sign();
+
+        return signature;
+    }
+
+    /***
+     * Asymmetric verify algorithm
+     * WydryszekWojciech implementation for verify signature
+     * @param message
+     * @param signature
+     * @param certificate
+     * @return boolean
+     * @throws NoSuchAlgorithmException
+     * @throws CertificateException
+     * @throws FileNotFoundException
+     * @throws SignatureException
+     * @throws KeyStoreException
+     * @throws UnrecoverableKeyException
+     * @throws InvalidKeyException
+     */
+    public boolean verify(byte[] message, byte[] signature, byte[] certificate) throws NoSuchAlgorithmException, CertificateException, FileNotFoundException, SignatureException, KeyStoreException, UnrecoverableKeyException, InvalidKeyException {
+
+        var signer = Signature.getInstance("SHA256withRSA");
+
+        signer.update(message);
+
+        var isValid = signer.verify(signature);
+
+        return isValid;
     }
 }
